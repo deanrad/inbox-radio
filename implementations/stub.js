65224e3f861fed4825f348852aa10b35b867c99a
Dean Radcliffe <dradcliffe@g2crowd.com>
2019-04-08 21:28:31 -0500

    WIP

--- implementations/stub.js
+++ implementations/stub.js
@@ -1,16 +1,19 @@
 const { after, from, concat, randomId } = require("rx-helper");
 
-const mult = process.env.TIME_SCALE = 0.10
+const mult = (process.env.TIME_SCALE = 0.1);
 function getMatchingMsgHeadersFromSearch() {
   return concat(
-    after(0, { subject: "Friday gig" }),
-    after(0, { subject: "Great jam" })
+    after(2000, { subject: "Friday gig" }),
+    after(2000, { subject: "Great jam" })
   );
 }
 function getMessageBodyFromHeader({ action }) {
-  return after(1500, {
+  return after(3000, {
     ...action.payload,
-    att: action.payload.subject === "Great jam" ? ["jam.mp3", "jam2.mp3", "jam3.mp3"] : []
+    att:
+      action.payload.subject === "Great jam"
+        ? ["jam.mp3", "jam2.mp3", "jam3.mp3"]
+        : []
   });
 }
 
@@ -20,11 +23,11 @@ function getAttachmentIdsFromBody({ action }) {
 
 function downloadAttachment({ action }) {
   return concat(
-    after(0, {
+    after(1500, {
       type: "net/att/start",
       payload: action.payload
     }),
-    after(5000*mult, {
+    after(5000 * mult, {
       type: "net/att/finish",
       payload: {
         ...action.payload,
@@ -37,7 +40,7 @@ function downloadAttachment({ action }) {
 function playFinishedAttachment({ action }) {
   return concat(
     after(0, { type: "player/play", payload: action.payload }),
-    after(5500*mult, { type: "player/complete", payload: action.payload })
+    after(5500, { type: "player/complete", payload: action.payload })
   );
 }
 
